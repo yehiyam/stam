332fd72ac7b463805578217195284d647c1e03f8
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-03 11:28:17 +0300

    fff

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
-echo ${DOCKER_HUB_PASS} | docker login --username yehiyam --password-stdin
-for REPO in ${CHANGED}
-do
-  echo ${REPO} changed. Running build
-  PRIVATE_REGISTRY=docker.io/yehiyam lerna run --scope $REPO build
-done
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo ${DOCKER_HUB_PASS} | docker login --username yehiyam --password-stdin
+  for REPO in ${CHANGED}
+  do
+    echo ${REPO} changed. Running build
+    PRIVATE_REGISTRY=docker.io/yehiyam lerna run --scope $REPO build
+  done
+else
+    echo "version skiped!"
+fi
